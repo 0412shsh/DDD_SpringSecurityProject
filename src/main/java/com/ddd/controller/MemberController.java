9230fdaadd38b90daa83f964d9a04558a3c31d7b
0412shsh <0412corn@naver.com>
2023-01-17 18:03:14 +0900

    [add] 회원 가입시 중복 체크, 웰컴 이메일 발송

--- src/main/java/com/ddd/controller/MemberController.java
+++ src/main/java/com/ddd/controller/MemberController.java
@@ -1,13 +1,20 @@
 package com.ddd.controller;
 
+import java.security.Principal;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.ddd.domain.AuthVO;
 import com.ddd.domain.MemberVO;
@@ -38,6 +45,7 @@ public class MemberController {
 	public String postRegister(MemberVO vo, AuthVO ao) throws Exception {
 		log.info("postRegister() 실행 ");
 		service.insertMember(vo, ao);
+		service.sendEmail(vo);
 		log.info("컨트롤러 : 회원가입 완료 ~!!! ");
 		return "redirect:/member/login";
 	}
@@ -58,5 +66,39 @@ public class MemberController {
 			model.addAttribute("logout","Logout!!");
 		}
 	}
+	
+	// 비밀번호 재발급 GET
+//	@GetMapping("/findpw")
+//	public void getFindPw() throws Exception {
+//		log.info("getFindPw() 호출 - 비밀번호 재발급");
+//	
+//	}
+	
+//	// 비밀번호 재발급 POST
+//	@PostMapping("/findpw")
+//	public void postFindPw(@ModelAttribute MemberVO member, HttpServletResponse response) throws Exception {
+//		log.info("postFindPw() 호출 - 비밀번호 재발급");
+//		service.findPw(member, response);
+//	
+//	}
+	
+	// 아이디 중복체크
+	@GetMapping("/idcheck")
+	@ResponseBody
+	public int idCheck(@RequestParam("userid") String userid) throws Exception {
+		log.info("idCheck() 호출");
+		int result = service.idCheck(userid);
+		return result; 
+	}
+	
+	// 이메일 중복체크
+	@GetMapping("/emailcheck")
+	@ResponseBody
+	public int emailCheck(@RequestParam("email") String email) throws Exception {
+		log.info("emailCheck() 호출 ");
+		int result = service.emailCheck(email);
+		return result;
+	}
+	
 	
 }
